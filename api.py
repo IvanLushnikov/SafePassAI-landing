9f049948166f559bac31802184d9f0acf00f5716
IvanLushnikov <ivanlusnikov159@gmail.com>
2025-04-11 17:01:30 +0500

    Update api.py

--- api.py
+++ api.py
@@ -18,7 +18,7 @@ from pathlib import Path
 knowledge_base = []
 try:
     base_path = Path(__file__).parent
-    kb_path = base_path / "knowledge_base.csv"
+kb_path = Path("/opt/render/project/knowledge_base.csv")
     with open(kb_path, encoding="utf-8") as f:
         reader = csv.DictReader(f, delimiter=";")
         for row in reader:
